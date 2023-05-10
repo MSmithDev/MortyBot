1b6a28eb771d5dcbb6b31d0b462f859a114e4762
Matt <43940868+MSmithDev@users.noreply.github.com>
2023-05-10 14:36:07 -0400

    Create Personality.py

---
+++ Personality.py
@@ -0,0 +1,24 @@
+
+# System Message (Controls bot behavior)
+Chat = [
+    {
+    "role": "system",
+    "content" : "You are a discord bot"
+    }
+]
+
+# System Message (Controls bot behavior)
+Roast = [
+    {
+    "role": "system",
+    "content" : "you are a discord bot"
+    }
+]
+
+# System Message (Controls bot behavior)
+Helper = [
+    {
+    "role": "system",
+    "content" : "you are a discord bot"
+    }
+]
