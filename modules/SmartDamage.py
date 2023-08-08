d2d391a917a7a3581e251246d3ea0dee9dfe5897
Matt <43940868+MSmithDev@users.noreply.github.com>
2023-08-08 14:13:28 -0400

    added logic for no results

--- modules/SmartDamage.py
+++ modules/SmartDamage.py
@@ -1,11 +1,11 @@
-import sqlite3
+import aiosqlite
 import json
 
 
 
 
 #This function is called by GPT to get the number of rounds needed to destroy a target
-def get_rounds_needed(sql, target, ammo, tier=0):
+async def get_rounds_needed(sql: aiosqlite.Connection, target: str, ammo: str, tier: int=0) -> json:
 
     #Build the query based on the parameters given
     if tier > 0:
@@ -17,29 +17,30 @@ def get_rounds_needed(sql, target, ammo, tier=0):
     print("[SQL Q] "+ query)
 
     #Execute the query
-    cursor = sql.cursor()
-    cursor.execute(query)
-
-    #Get the results
-    row = cursor.fetchone()
-
+    async with sql.cursor() as cursor:
+        await cursor.execute(query)
+        row = await cursor.fetchone()
+    
     #get tier from the query
-    tier = row[2]
+    if row is not None:
+        tier = row[2]
 
-    #if the tier is greater than 0, then we need to return the tier
-    if (tier > 0):
-        response = {
+        #if the tier is greater than 0, then we need to return the tier
+        if (tier > 0):
+            response = {
+                    "target": row[1],
+                    "ammo": ammo,
+                    "Tier": tier,
+                    "needed": row[0],
+                }
+        else:
+            response = {
                 "target": row[1],
                 "ammo": ammo,
-                "Tier": tier,
                 "needed": row[0],
             }
-    else:
-        response = {
-            "target": row[1],
-            "ammo": ammo,
-            "needed": row[0],
-        }
 
-    #return the response as a JSON string
-    return json.dumps(response)
+        #return the response as a JSON string
+        return json.dumps(response)
+    else:
+        return json.dumps({"error": "No results found"})
