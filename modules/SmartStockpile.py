e6e503cb77012b1d0e19f38bb9a7019e03b7b051
Matt <43940868+MSmithDev@users.noreply.github.com>
2023-08-08 16:15:49 -0400

    added code and timestamp

--- modules/SmartStockpile.py
+++ modules/SmartStockpile.py
@@ -2,6 +2,7 @@ import discord
 from dataclasses import dataclass
 from typing import List
 from collections import defaultdict
+import modules.utils as utils
 @dataclass
 class Stockpile:
     guild: int
@@ -9,6 +10,7 @@ class Stockpile:
     townname: str
     stockpileid: int
     location: str
+    code: int
     expires: int
     created: str
     last: str
@@ -18,7 +20,7 @@ async def getGuildStockpiles(sql, guild):
 
     #Build the query based on the parameters given
     query = f"""
-    SELECT stockpiles.townid, towns.name, stockpiles.stockpileid, stockpiles.location,
+    SELECT stockpiles.townid, towns.name, stockpiles.stockpileid, stockpiles.location, stockpiles.code,
     stockpiles.expires, stockpiles.createdby, stockpiles.lastperson
     FROM stockpiles INNER JOIN towns ON stockpiles.townid=towns.townid
     WHERE stockpiles.guild={guild}
@@ -35,29 +37,31 @@ async def getGuildStockpiles(sql, guild):
     
     #Loop through the results and create a stockpile object for each
     for row in rows:
-        stockpiles.append(Stockpile(guild,row[0],row[1],row[2],row[3],row[4],row[5],row[6]))
+        stockpiles.append(Stockpile(guild,row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7]))
 
     return stockpiles
 
-async def makeStockpileEmbeds(stockpile: List[Stockpile], channel):
-
+async def makeStockpileEmbeds(stockpile: List[Stockpile], interaction: discord.Interaction):
+    await interaction.response.send_message("Testing", ephemeral=True)
     stockpiles_by_town = defaultdict(list)
     for pile in stockpile:
         stockpiles_by_town[pile.townid].append(pile)
 
     for townid, town_stockpiles in stockpiles_by_town.items():
-        embed = discord.Embed(title=f"Stockpiles in {town_stockpiles[0].townname}")
+        title = f"Stockpiles in {town_stockpiles[0].townname}"
+        print("[SmartStockpile] Title before: " + str(len(title)))
+        print("[SmartStockpile] Title after: " + str(len(utils.padEmbed(title))))
+        embed = discord.Embed(title=utils.padEmbed(title))
 
         # Prepare the values for each field
         locations = '\n'.join([pile.location for pile in town_stockpiles])
-        codes = '\n'.join([str(pile.stockpileid) for pile in town_stockpiles])
-        expires = '\n'.join([str(pile.expires) for pile in town_stockpiles])  # You might need to format this differently
+        codes = '\n'.join([str(pile.code) for pile in town_stockpiles])
+        expires = '\n'.join([utils.discordTimestamp(pile.expires) for pile in town_stockpiles])  # You might need to format this differently
 
         embed.add_field(name="Location", value=locations)
         embed.add_field(name="Code", value=codes)
         embed.add_field(name="Expires", value=expires)
-
-        await channel.send(embed=embed)
+        await interaction.channel.send(embed=embed)
     return "todo"
 
 
